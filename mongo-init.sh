b7ca8b4bb40f5c4e35def2447207cce957ebedd2
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-07 20:07:10 +0200

    Add files via upload

---
+++ mongo-init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mongo -- "${MONGO_INITDB_DATABASE:-${DATABASE_NAME}}" <<EOF
+db.createUser(
+    {
+        user: "${MONGO_INITDB_ROOT_USERNAME:-${DATABASE_USERNAME}}",
+        pwd: "$(printf %s "${MONGO_INITDB_ROOT_PASSWORD:-$(cat "${DATABASE_PASSWORD_FILENAME}")}")",
+        roles: ["readWrite"]
+    }
+);
+EOF
